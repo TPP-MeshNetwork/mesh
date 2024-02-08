ab39d4143a26b7ff4acec5d52e420b2e11e45b03
Eliana <elianagam2@gmail.com>
2024-02-07 21:18:38 -0300

    Create file .h

---
+++ main/mqtt/aws_utils.h
@@ -0,0 +1,283 @@
+/* Standard includes. */
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+/* MQTT API headers. */
+#include "core_mqtt.h"
+#include "core_mqtt_state.h"
+
+#include "mqtt/publish_packets.h"
+
+/**
+ * @brief Sends an MQTT UNSUBSCRIBE to unsubscribe from
+ * #MQTT_EXAMPLE_TOPIC defined at the top of the file.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ *
+ * @return EXIT_SUCCESS if UNSUBSCRIBE was successfully sent;
+ * EXIT_FAILURE otherwise.
+ */
+static int unsubscribeFromTopic( MQTTContext_t * pMqttContext );
+
+/**
+ * @brief Sends an MQTT PUBLISH to #MQTT_EXAMPLE_TOPIC defined at
+ * the top of the file.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ *
+ * @return EXIT_SUCCESS if PUBLISH was successfully sent;
+ * EXIT_FAILURE otherwise.
+ */
+static int publishToTopic( MQTTContext_t * pMqttContext, char * message);
+
+
+/**
+ * @brief Function to clean up an outgoing publish at given index from the
+ * #outgoingPublishPackets array.
+ *
+ * @param[in] index The index at which a publish message has to be cleaned up.
+ */
+static void cleanupOutgoingPublishAt( uint8_t index );
+
+/**
+ * @brief Function to clean up all the outgoing publishes maintained in the
+ * array.
+ */
+static void cleanupOutgoingPublishes( void );
+
+/**
+ * @brief Function to clean up the publish packet with the given packet id.
+ *
+ * @param[in] packetId Packet identifier of the packet to be cleaned up from
+ * the array.
+ */
+static void cleanupOutgoingPublishWithPacketID( uint16_t packetId );
+
+/**
+ * @brief Function to resend the publishes if a session is re-established with
+ * the broker. This function handles the resending of the QoS1 publish packets,
+ * which are maintained locally.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ */
+static int handlePublishResend( MQTTContext_t * pMqttContext );
+
+/**
+ * @brief Function to update variable globalSubAckStatus with status
+ * information from Subscribe ACK. Called by eventCallback after processing
+ * incoming subscribe echo.
+ *
+ * @param[in] Server response to the subscription request.
+ */
+static void updateSubAckStatus( MQTTPacketInfo_t * pPacketInfo );
+
+/**
+ * @brief Function to handle resubscription of topics on Subscribe
+ * ACK failure. Uses an exponential backoff strategy with jitter.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ */
+static int handleResubscribe( MQTTContext_t * pMqttContext );
+
+/**
+ * @brief The function to handle the incoming publishes.
+ *
+ * @param[in] pPublishInfo Pointer to publish info of the incoming publish.
+ * @param[in] packetIdentifier Packet identifier of the incoming publish.
+ */
+static void handleIncomingPublish( MQTTPublishInfo_t * pPublishInfo,
+                                   uint16_t packetIdentifier );
+
+/**
+ * @brief Wait for an expected ACK packet to be received.
+ *
+ * This function handles waiting for an expected ACK packet by calling
+ * #MQTT_ProcessLoop and waiting for #mqttCallback to set the global ACK
+ * packet identifier to the expected ACK packet identifier.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] usPacketIdentifier Packet identifier for expected ACK packet.
+ * @param[in] ulTimeout Maximum duration to wait for expected ACK packet.
+ *
+ * @return true if the expected ACK packet was received, false otherwise.
+ */
+static int waitForPacketAck( MQTTContext_t * pMqttContext,
+                             uint16_t usPacketIdentifier,
+                             uint32_t ulTimeout );
+/**
+ * @brief Function to clean up an outgoing publish at given index from the
+ * #outgoingPublishPackets array.
+ *
+ * @param[in] index The index at which a publish message has to be cleaned up.
+ */
+static void cleanupOutgoingPublishAt( uint8_t index );
+                             
+
+/**
+ * @brief The application callback function for getting the incoming publish
+ * and incoming acks reported from MQTT library.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] pPacketInfo Packet Info pointer for the incoming packet.
+ * @param[in] pDeserializedInfo Deserialized information from the incoming packet.
+ */
+static void eventCallback( MQTTContext_t * pMqttContext,
+                           MQTTPacketInfo_t * pPacketInfo,
+                           MQTTDeserializedInfo_t * pDeserializedInfo );
+
+
+/**
+ * @brief Close an MQTT session by sending MQTT DISCONNECT.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ *
+ * @return EXIT_SUCCESS if DISCONNECT was successfully sent;
+ * EXIT_FAILURE otherwise.
+ */
+static int disconnectMqttSession( MQTTContext_t * pMqttContext );
+
+/**
+ * @brief Initializes the MQTT library.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] pNetworkContext The network context pointer.
+ *
+ * @return EXIT_SUCCESS if the MQTT library is initialized;
+ * EXIT_FAILURE otherwise.
+ */
+static int initializeMqtt( MQTTContext_t * pMqttContext,
+                           NetworkContext_t * pNetworkContext );
+
+/**
+ * @brief Connect to MQTT broker with reconnection retries.
+ *
+ * If connection fails, retry is attempted after a timeout.
+ * Timeout value will exponentially increase until maximum
+ * timeout value is reached or the number of attempts are exhausted.
+ *
+ * @param[out] pNetworkContext The output parameter to return the created network context.
+ * @param[out] pMqttContext The output to return the created MQTT context.
+ * @param[in,out] pClientSessionPresent Pointer to flag indicating if an
+ * MQTT session is present in the client.
+ * @param[out] pBrokerSessionPresent Session was already present in the broker or not.
+ * Session present response is obtained from the CONNACK from broker.
+ *
+ * @return EXIT_FAILURE on failure; EXIT_SUCCESS on successful connection.
+ */
+static int connectToServerWithBackoffRetries( NetworkContext_t * pNetworkContext,
+                                              MQTTContext_t * pMqttContext,
+                                              bool * pClientSessionPresent,
+                                              bool * pBrokerSessionPresent );
+
+/**
+ * @brief Function to clean up all the outgoing publishes maintained in the
+ * array.
+ */
+static void cleanupOutgoingPublishes( void );
+
+/**
+ * @brief Function to update variable globalSubAckStatus with status
+ * information from Subscribe ACK. Called by eventCallback after processing
+ * incoming subscribe echo.
+ *
+ * @param[in] Server response to the subscription request.
+ */
+static void updateSubAckStatus( MQTTPacketInfo_t * pPacketInfo );
+
+/**
+ * @brief Sends an MQTT CONNECT packet over the already connected TCP socket.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] createCleanSession Creates a new MQTT session if true.
+ * If false, tries to establish the existing session if there was session
+ * already present in broker.
+ * @param[out] pSessionPresent Session was already present in the broker or not.
+ * Session present response is obtained from the CONNACK from broker.
+ *
+ * @return EXIT_SUCCESS if an MQTT session is established;
+ * EXIT_FAILURE otherwise.
+ */
+static int establishMqttSession( MQTTContext_t * pMqttContext,
+                                 bool createCleanSession,
+                                 bool * pSessionPresent );
+
+/**
+ * @brief Function to clean up the publish packet with the given packet id.
+ *
+ * @param[in] packetId Packet identifier of the packet to be cleaned up from
+ * the array.
+ */
+static void cleanupOutgoingPublishWithPacketID( uint16_t packetId );
+
+/**
+ * @brief Function to clean up an outgoing publish at given index from the
+ * #outgoingPublishPackets array.
+ *
+ * @param[in] index The index at which a publish message has to be cleaned up.
+ */
+static void cleanupOutgoingPublishAt( uint8_t index );
+
+/**
+ * @brief Sends an MQTT PUBLISH to #MQTT_EXAMPLE_TOPIC defined at
+ * the top of the file.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ *
+ * @return EXIT_SUCCESS if PUBLISH was successfully sent;
+ * EXIT_FAILURE otherwise.
+ */
+static int publishToTopic( MQTTContext_t * pMqttContext, char * message);
+
+/**
+ * @brief Function to clean up an outgoing publish at given index from the
+ * #outgoingPublishPackets array.
+ *
+ * @param[in] index The index at which a publish message has to be cleaned up.
+ */
+static void cleanupOutgoingPublishAt( uint8_t index );
+
+/**
+ * @brief Function to get the free index at which an outgoing publish
+ * can be stored.
+ *
+ * @param[out] pIndex The output parameter to return the index at which an
+ * outgoing publish message can be stored.
+ *
+ * @return EXIT_FAILURE if no more publishes can be stored;
+ * EXIT_SUCCESS if an index to store the next outgoing publish is obtained.
+ */
+static int getNextFreeIndexForOutgoingPublishes( uint8_t * pIndex );
+
+/**
+ * @brief Wait for an expected ACK packet to be received.
+ *
+ * This function handles waiting for an expected ACK packet by calling
+ * #MQTT_ProcessLoop and waiting for #mqttCallback to set the global ACK
+ * packet identifier to the expected ACK packet identifier.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] usPacketIdentifier Packet identifier for expected ACK packet.
+ * @param[in] ulTimeout Maximum duration to wait for expected ACK packet.
+ *
+ * @return true if the expected ACK packet was received, false otherwise.
+ */
+static int waitForPacketAck( MQTTContext_t * pMqttContext,
+                             uint16_t usPacketIdentifier,
+                             uint32_t ulTimeout );
+
+
+/**
+ * @brief Call #MQTT_ProcessLoop in a loop for the duration of a timeout or
+ * #MQTT_ProcessLoop returns a failure.
+ *
+ * @param[in] pMqttContext MQTT context pointer.
+ * @param[in] ulTimeoutMs Duration to call #MQTT_ProcessLoop for.
+ *
+ * @return Returns the return value of the last call to #MQTT_ProcessLoop.
+ */
+static MQTTStatus_t processLoopWithTimeout( MQTTContext_t * pMqttContext,
+                                            uint32_t ulTimeoutMs );
+
